8fda59a5c41ea398ba755b27568caaff9f4535e8
Annet Martínez <l24121399@morelia.tecnm.mx>
2025-04-08 22:28:10 -0600

    Initial commit

--- src/com/actividad/herencia/practica/AnnetJME/poo/Acuatico.java
+++ src/com/actividad/herencia/practica/AnnetJME/poo/Acuatico.java
@@ -1,5 +1,18 @@
 package com.actividad.herencia.practica.AnnetJME.poo;
 import java.util.*;
 public class Acuatico extends Vehiculo{
+    private String tipoEmbarcacion;
+    private int numMotores;
+    private int tripulacion;
+    private int numCubiertas;
+    public Acuatico(int numMotores, int trpulacion, int numCubiertas, String marca, int modeloAnio, int capacidadPasajeros, int velocidadIn, int velocidadFin, String combustible, boolean estadoVehi, int tiempo, int tamanio){
+        super(marca, modeloAnio, capacidadPasajeros, velocidadIn, velocidadFin, combustible, estadoVehi, tiempo, tamanio);
+        this.tipoEmbarcacion=tipoEmbarcacion;
+        this.numMotores=numMotores;
+        this.tripulacion=tripulacion;
+        this.numCubiertas=numCubiertas;
+
+
+    }
 
 }
