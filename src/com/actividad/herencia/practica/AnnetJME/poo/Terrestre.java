dd49a28c11c11a3f6ee561212812073e3742be0a
Annet Martínez <l24121399@morelia.tecnm.mx>
2025-04-08 22:14:48 -0600

    Initial commit

--- src/com/actividad/herencia/practica/AnnetJME/poo/Terrestre.java
+++ src/com/actividad/herencia/practica/AnnetJME/poo/Terrestre.java
@@ -1,4 +1,18 @@
 package com.actividad.herencia.practica.AnnetJME.poo;
 import java.util.*;
-public class Terrestre {
+public class Terrestre extends Vehiculo{
+    private int numRuedas;
+    private String transmision;
+    private String tipoVehiculo;
+    public Terrestre(int numRuedas, String transmision, String tipoVehiculo, String marca, int modeloAnio, int capacidadPasajeros, int velocidadIn, int velocidadFin, String combustible, boolean estadoVehi, int tiempo, int tamanio){
+        super(marca, modeloAnio, capacidadPasajeros, velocidadIn, velocidadFin, combustible, estadoVehi, tiempo, tamanio);
+        this.numRuedas=numRuedas;
+        this.transmision=transmision;
+        this.tipoVehiculo=tipoVehiculo;
+    }
+    public void mostrarTerrestre(){
+        System.out.println("El numero de ruedas de este vehiculo es: "+numRuedas);
+        System.out.println("El tipo de transmision de carro es: " + transmision);
+        System.out.println("El tipo de vehiculo es: "+ tipoVehiculo);
+    }
 }
--- src/com/actividad/herencia/practica/AnnetJME/poo/Vehiculo.java
+++ src/com/actividad/herencia/practica/AnnetJME/poo/Vehiculo.java
@@ -1,4 +1,46 @@
 package com.actividad.herencia.practica.AnnetJME.poo;
 import java.util.*;
-public class Vehiculo {
+public class Vehiculo {// ESTA SERIA LA CLASE PADRE.
+    private String marca;
+    private int modeloAnio;
+    private int capacidadPasajeros;
+    private int velocidadIn;
+    private int velocidadFin;
+    private String combustible;
+    private boolean estadoVehi;
+    private int tiempo;
+    private int tamanio;
+    public Vehiculo(String marca, int modeloAnio, int capacidadPasajeros, int velocidadIn, int velocidadFin, String combustible, boolean estadoVehi, int tiempo, int tamanio){
+        this.marca=marca;
+        this.modeloAnio=modeloAnio;
+        this.capacidadPasajeros=capacidadPasajeros;
+        this.velocidadIn =velocidadIn;
+        this.velocidadFin=velocidadFin;
+        this.combustible=combustible;
+        this.estadoVehi=0;
+        this.tiempo=tiempo;
+        this.tamanio=tamanio;
+    }
+    public void mostrarVehiculo(){
+        System.out.println("La marca del vehículo es: " + marca);
+        System.out.println("El año modelo del vehículo es: " + modeloAnio);
+        System.out.println("La máxima capacidad de pasajeros del vehículo es: " + capacidadPasajeros);
+        System.out.println("La máxima velocidadIn es: " + velocidadIn);
+        System.out.println("El combustible del vehículo es: " + combustible);
+        System.out.println("el tamaño del vahícuolo es: "+ tamanio);
+    }
+    public void arrancar(){
+        if estadoVehi==0{
+            this.estadoVehi=estadoVehi++;
+        }
+    }
+    public void detener(){
+        if estadoVehi==1{
+            this.estadoVehi=estadoVehi--;
+        }
+    }
+    public int acelerar(int aceleracion){
+        aceleracion=(velocidadFin-velocidadIn)/tiempo;
+    }
+
 }
